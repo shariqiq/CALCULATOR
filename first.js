b04d70fff77a81c45bf2dab69cd16dbd207a9dfa
shariqiq <shariqhashmi541@gmail.com>
2024-07-23 19:31:49 +0500

    Calculator

---
+++ first.js
@@ -0,0 +1,142 @@
+//var sh = {
+//     a: 'shariq',
+//     b: 'hashmi',
+//     age: '24',
+//     city: "karachi"
+// };
+
+// console.log(sh.a, sh.b, sh.age, sh.city);
+
+// let school  =(" my school name is the creator school");
+// let collage =("my collage name is premiere degree collage  ");
+// console .log ("school collage");
+
+// let myname = ('shariq code');
+// console.log(myname);
+
+// var firstname =("shariq hashmi");
+// console.log(firstname);
+
+// let myname = "myfavouritecolourisblack";
+// console.log(myname);
+
+// var userid = {
+// personname : 'shariq' ,
+//   secondname : 'hashmi',
+//  age : 24,
+//  email: 'shariqhashmi542gmail.com'}
+// console.log (userid);
+
+// let multi= 10 > 5;
+// console.log(multi);
+
+// let  b = 10 ;
+// console.log( typeof b);
+// let b;
+// console.log (b);
+
+// let  student = {
+//     name: 'shariq',
+//     nick: 'hahsmi',
+//     email : 'shariqiqbal541@gmail.com',
+//     num:  315852286,
+//     age: 24,
+//     degree:' inter',
+
+
+
+
+
+// date //18/7/2014 day//thursday
+// 1st task complete program declair a primary number 
+
+
+
+
+    //   const number = 4;
+
+    //   if (number <= 1) {
+    //       console.log(`${number}
+    //          prime number nahi hai.`);
+    //   } else {
+    //       let isPrime = true;
+    //       for (let i = 2; 
+    //         i <= (number); i++) {
+    //           if (number % i === 0) {
+    //               isPrime = false;  
+    //           }
+    //       }
+    //       if (isPrime) {
+    //           console.log(`${number}
+    //              ek prime number hai.`);
+    //       } else {
+    //           console.log(`${number}
+    //              prime number nahi hai.`);
+    //       }
+    //   }
+  
+
+
+
+// day/friday/date/19/07/2024
+// second task comlete progrm declair a armstrong number 
+
+
+
+
+
+
+
+
+    //   let x = 153;
+    //   let sum = 0;
+    //   let temp = x;
+      
+    //   while (temp > 0) {
+    //       let y = temp % 10;
+    //       sum += y ** 3;
+    //       temp = parseInt(temp / 10);
+    //   }
+    //   console.log("Original number:", x);
+    //   console.log("Sum of cubes of digits:", sum);
+      
+    //   if (x === sum) {
+    //       console.log('Yes, Armstrong number');
+    //   } else {
+    //       console.log(' not, Armstrong number');
+    //   }
+
+
+
+
+
+    // let a= 0; b= 1;
+    // console.log(a);
+    
+    // 3rd task complete make a program financci series
+    
+    // console.log(b);
+    // for(let i=0;i<=10;i++){
+    //  let temp = a+b
+    //  console.log(temp);
+    //  a=b;
+    //  b=temp;
+    //     }
+    // function run
+// function  myname(){
+//     console.log ("shariq");
+//     console.log("hashmi")}
+     
+//      myname()
+// myname()
+
+// 4th task making a program calculator//
+
+const a ={
+    name : "shariq",
+    lastname : "hashmi",
+    age :24,
+    number: 3158522860,
+}
+
+console.log(a);
